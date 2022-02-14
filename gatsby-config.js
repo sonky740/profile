f2e25a08675364bbddce01bc7cb362c9c84ef746
손기연 <sonky740@minwise.co.kr>
2022-02-14 15:11:42 +0900

    sourcemaps delete

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-no-sourcemaps",
     {
       resolve: "gatsby-source-filesystem",
       options: {
