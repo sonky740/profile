eb5596d80ea48185dee9516b8699dfaa321c3140
손기연 <sonky740@minwise.co.kr>
2022-02-18 17:52:12 +0900

    AboutMe ing

--- src/components/sources/aboutMe.jsx
+++ src/components/sources/aboutMe.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ScrollAni } from '../index';
 
 const Wrap = styled.div``;
 
@@ -10,14 +11,29 @@ const MainMsg = styled.p`
 `;
 
 const SubMsg = styled.p`
-  font-size: 2rem;
-`
+  font-size: 1.8rem;
+
+  & + & {
+    margin-top: 1rem;
+  }
+`;
 
 export default function AboutMe() {
   return (
     <Wrap>
-      <MainMsg>프론트엔드를 지향하는<br />욕심 많은 퍼블리셔 손기연입니다!</MainMsg>
-      <SubMsg>이력서를 보셨겠지만 다시 한 번 제 약력을 소개하겠습니다!</SubMsg>
+      <ScrollAni>
+        <MainMsg>
+          프론트엔드를 지향하는
+          <br />
+          욕심 많은 퍼블리셔 손기연입니다!
+        </MainMsg>
+      </ScrollAni>
+      <ScrollAni>
+        <SubMsg>저는 서버 엔지니어로 시작해서, 사내 인프라, 다음은 5년넘게 퍼블리셔로 활동중입니다.</SubMsg>
+        <SubMsg>UI가 정적인 화면보다는 동적인 모션을 좋아해서 늘 모션을 어떻게 줄까 고민하고, 코드의 효율성을 위해서 컴포넌트 단위로 최적화된 마크업과 스크립트 작성에 노력합니다. 또한, UX적인 측면도 생각하여 항상 키보드 이벤트와 최소한의 접근성도 고려합니다.</SubMsg>
+        <SubMsg>저는 제 일이 재밌습니다. 재밌으니까 좋아하고 욕심이 생겨서 더 많은 걸 배워보고 싶습니다.</SubMsg>
+        <SubMsg>재밌게 일하는 것, 제가 추구하는 목표입니다.</SubMsg>
+      </ScrollAni>
     </Wrap>
   );
 }
