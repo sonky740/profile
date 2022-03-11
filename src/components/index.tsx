cb5f32550799f756992a579e13048341b9b9527c
손기연 <sonky740@minwise.co.kr>
2022-03-11 10:50:57 +0900

    .

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -13,7 +13,3 @@ export { default as LabelText } from './sources/labelText';
 export { default as ScrollAni } from './sources/scrollAnimation';
 export { default as ScrollDetail } from './sources/scrollDetail';
 export { default as AboutMe } from './sources/aboutMe';
-export { default as Accordion } from './sources/accordion/accordion';
-export { default as AccordionItem } from './sources/accordion/accordionItem';
-
-export { default as Accor } from './sources/accor/accordion';
